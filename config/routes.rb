4178bd2930fffb39f9655535325994b60fc0df2f
kazuuu <marcellokazuo@gmail.com>
2012-10-20 13:34:35 -0300

    Finish Layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Pickone::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-
-
   resources :draws
-
   resources :users, :user_sessions
+
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
   
