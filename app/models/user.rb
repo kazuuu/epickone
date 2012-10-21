64360703b7c25b69b56465559bc26328e1044885
kazuuu <marcellokazuo@gmail.com>
2012-10-21 14:31:56 -0200

    Delete Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,27 @@ class User < ActiveRecord::Base
                   :zipcode,
                   :phone_mobile,
                   :admin_flag,
-                  :avatar
+                  :avatar,
+                  :avatar_delete
                   
+    has_attached_file :avatar
+    before_save :destroy_avatar?
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_many :draws, dependent: :destroy
   acts_as_authentic do |c| 
     c.login_field = :email 
     c.require_password_confirmation = false
   end 
-  
+  def avatar_delete
+      @avatar_delete ||= "0"
+    end
+
+    def avatar_delete=(value)
+      @avatar_delete = value
+    end
+
+  private
+    def destroy_avatar?
+      self.avatar.clear if @avatar_delete == "1"
+    end
 end
