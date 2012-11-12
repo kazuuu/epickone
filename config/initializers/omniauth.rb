5d0128dda916f7e2661150ae0004f0fe6e657747
kazuuu <marcellokazuo@gmail.com>
2012-11-12 11:20:09 -0200

    Facebook:profile create update 4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'Consumer key', 'Consumer secret'
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_birthday,read_stream,first_name,last_name,birthday'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'user_birthday'
   
 end
