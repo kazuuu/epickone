2b77088a0d75be918dc112e34023cc7cf13f804a
kazuuu <marcellokazuo@gmail.com>
2012-11-12 10:42:16 -0200

    Facebook:profile create update 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -114,7 +114,7 @@ class User < ActiveRecord::Base
         :crypted_password => "facebook",
         :password_salt => "facebook",
         :persistence_token => "facebook",
-        :birth_date   => auth_hash["extra"]["raw_info"]["birthday"]
+        :birth_date => Date.strptime( auth_hash["extra"]["raw_info"]["birthday"],'%m/%d/%Y')
       })
     end
 
@@ -136,7 +136,7 @@ class User < ActiveRecord::Base
         :avatar_url => auth_hash["info"]["image"],
         :first_name => auth_hash["info"]["first_name"],
         :last_name => auth_hash["info"]["last_name"],
-        :birth_date   => auth_hash["extra"]["raw_info"]["birthday"]
+        :birth_date => Date.strptime( auth_hash["extra"]["raw_info"]["birthday"],'%m/%d/%Y')
       })
     end
 # End Omniauth
