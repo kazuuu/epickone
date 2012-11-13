06c065b03645aaa95324a2ea2b310b1c533a7d18
kazuuu <marcellokazuo@gmail.com>
2012-11-13 21:46:15 -0200

    Facebook: Graph 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'Consumer key', 'Consumer secret'
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_birthday,user_location,user_hometown,publish_stream'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_birthday,user_location,user_hometown'
   
 end
