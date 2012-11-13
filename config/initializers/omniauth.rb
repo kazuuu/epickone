67ea21b66f299fe00a74a376eb21f481421c79ff
kazuuu <marcellokazuo@gmail.com>
2012-11-13 15:02:03 -0200

    Facebook: User

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'Consumer key', 'Consumer secret'
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'user_birthday,user_location,user_hometown'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_birthday,user_location,user_hometown'
   
 end
