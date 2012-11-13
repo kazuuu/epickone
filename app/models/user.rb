d5c7736985412d2422ed3f9aa72883d51fe59250
kazuuu <marcellokazuo@gmail.com>
2012-11-12 23:38:46 -0200

    Facebook: User

--- app/models/user.rb
+++ app/models/user.rb
@@ -127,7 +127,7 @@ class User < ActiveRecord::Base
       birthday = nil
     end
     self.update_attributes({
-      :country => auth_hash["extra"]["raw_info"]["location"],
+      :country => auth_hash["extra"]["raw_info"]["location"]["name"],
       :birthday => birthday, 
 
       :facebook_uid => auth_hash["uid"],
