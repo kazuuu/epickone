e8913fffbdcf1693613fe322e774a997d7bd739b
kazuuu <marcellokazuo@gmail.com>
2012-11-14 20:32:59 -0200

    Delayed_job_active_record

--- app/controllers/draws_controller.rb
+++ app/controllers/draws_controller.rb
@@ -141,7 +141,7 @@ class DrawsController < ApplicationController
     session[:draw_id] = params[:id]
     @draw = Draw.find(params[:id])
     
-    current_user.share_join(current_user.id, draw_url(@draw))
+    User.delay.share_join(current_user.id, draw_url(@draw))
     
     @numbers = (1..1000).to_a.paginate(page: params[:page], :per_page => 100)
 
