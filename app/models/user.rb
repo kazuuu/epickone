dcdb6c800fcef97073f20858cd68c2089ff4cb14
kazuuu <marcellokazuo@gmail.com>
2012-11-13 22:59:27 -0200

    Facebook: Graph 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -165,6 +165,19 @@ class User < ActiveRecord::Base
 
 # End Omniauth
 
+# Koala Gem
+def facebook
+  @facebook ||= Koala::Facebook::API.new(oauth_token)
+  block_given? ? yield(@facebook) : @facebook
+rescue Koala::Facebook::APIError => e
+  logger.info e.to_s
+  nil
+end
+
+def friends_count
+  facebook { |fb| fb.get_connection("me", "friends").size }
+end
+# End Koala
 
 # Paperclip for Images
     
