69b92a853f8c20b11aadb5339383fd9d49226d7b
kazuuu <marcellokazuo@gmail.com>
2012-11-15 16:29:43 -0200

    Facebook: OpenGraph without Delay Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -170,7 +170,6 @@ end
 def self.share_join(user_id, draw_url)
   user = User.find(user_id)
   user.facebook.put_connections("me", "epickone:joined", :game => draw_url)
-#  user.facebook.put_connections("me", "feed", :message => "I am writing on my wall!")
 end
 
 # End Koala
