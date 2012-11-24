abe0700c63ffdf6fc3bdd10ab35fe7d31647aff0
kazuuu <marcellokazuo@gmail.com>
2012-11-24 00:59:04 -0200

    Facebook Welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,7 +160,6 @@ class User < ActiveRecord::Base
       :password_salt => "facebook",
       :persistence_token => "facebook"
     })
-    self.deliver_activation!
   end
   def update_user_from_facebook(auth_hash)
     if auth_hash.extra.raw_info.birthday != ""
