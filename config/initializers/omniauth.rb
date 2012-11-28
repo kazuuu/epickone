f016bc6c0b577824f2ed43fa8ec3fb34dd6e253a
kazuuu <marcellokazuo@gmail.com>
2012-11-28 19:29:09 -0200

    Facebook Image

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do  
 
   provider :twitter, 'Consumer key', 'Consumer secret'
-  provider :facebook
-            , ENV['FACEBOOK_APP_ID']
-            , ENV['FACEBOOK_SECRET']
-            , :scope => 'email,user_birthday,user_location,user_hometown,publish_stream,publish_actions'
-            , :image_size => 'normal'
+  provider :facebook, 
+            ENV['FACEBOOK_APP_ID'],
+            ENV['FACEBOOK_SECRET'],
+            :scope => 'email,user_birthday,user_location,user_hometown,publish_stream,publish_actions',
+            :image_size => 'normal'
 end
