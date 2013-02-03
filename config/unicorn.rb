536ff576d49baea0c5d2d37e96d59821f63aecea
kazuuu <marcellokazuo@gmail.com>
2013-02-03 15:57:01 -0200

    unicorn heroku try fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,2 @@
-worker_processes 3 # amount of unicorn workers to spin up
+worker_processes 2 # amount of unicorn workers to spin up
 timeout 30
-before_exec do |server| 
-  ENV["BUNDLE_GEMFILE"] = "~/Sites/epickone/Gemfile" 
-end
