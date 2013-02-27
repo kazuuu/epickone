22662ae000bfa0d6689e2f1ad7b4357609976eb0
kazuuu <marcellokazuo@gmail.com>
2013-02-26 23:24:19 -0300

    Unicorn Perform

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+#web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+web: bundle exec unicorn_rails -p $PORT -c ./config/unicorn.rb
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,12 @@
-worker_processes 2 # amount of unicorn workers to spin up
+# If you have a very small app you may be able to
+# increase this, but in general 3 workers seems to
+# work best
+worker_processes 3
+
+# Load your app into the master before forking
+# workers for super-fast worker spawn times
+preload_app true
+
+# Immediately restart any workers that
+# haven't responded within 30 seconds
 timeout 30
