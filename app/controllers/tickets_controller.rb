be80d8bd532eb921bd3c9803d9ebc3e05f1a8eaa
kazuuu <marcellokazuo@gmail.com>
2013-02-27 01:40:50 -0300

    Ticket Delete redirect

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,6 +2,7 @@ class TicketsController < InheritedResources::Base
   def destroy
     @ticket = Ticket.find(params[:id])
     @ticket.destroy
-    redirect_to checkout_cart_path(:id => session[:event_id])
+#    redirect_to checkout_cart_path(:id => session[:event_id])
+    redirect_to checkout_cart_path(current_cart)
   end
 end
