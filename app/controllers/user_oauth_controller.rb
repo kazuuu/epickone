61de047d722438358ddd9c5b6d7c04d9c155821d
kazuuu <marcellokazuo@gmail.com>
2013-02-27 03:09:37 -0300

    Heroku encoding UTF problem fix

--- app/controllers/user_oauth_controller.rb
+++ app/controllers/user_oauth_controller.rb
@@ -7,7 +7,7 @@ class UserOauthController < ApplicationController
       aTeste = env["omniauth.auth"]
       
       if aTeste.provider == 'facebook'
-        @current_user = User.find_or_create_from_oauth(env["omniauth.auth"].to_utf8)
+        @current_user = User.find_or_create_from_oauth(env["omniauth.auth"])
       elsif aTeste.provider == 'twitter'
         current_user.update_attributes({
           :twitter_uid => aTeste["uid"],
