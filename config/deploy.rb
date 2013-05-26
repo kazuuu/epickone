03c968120930c4d487660e229a875d6caaf42fba
kazuuu <marcellokazuo@gmail.com>
2013-05-26 21:23:20 +0100

    Capistrano Task DB by kaz

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,4 +43,24 @@ namespace :deploy do
  end
 end
 
+namespace :db do
+  desc "Snapshots production db and dumps into local development db"
+  task :pull, roles: :db, only: { primary: true } do
+    # adjust prod_config to point to your database.yml
+    prod_config = capture "cat #{shared_path}/config/database.yml"
+
+    prod = YAML::load(prod_config)["production"]
+    dev  = YAML::load_file("config/database.yml")["development"]
+    dump = "/tmp/#{Time.now.to_i}-#{application}.psql"
+
+    run %{pg_dump -x -Fc #{prod["database"]} -f #{dump}}
+    get dump, dump
+    run "rm #{dump}"
+
+    system %{dropdb #{dev["database"]}}
+    system %{createdb #{dev["database"]} -O #{dev["username"]}}
+    system %{pg_restore -O -U #{dev["username"]} -d #{dev["database"]} #{dump}}
+    system "rm #{dump}"
+  end
+end
 
