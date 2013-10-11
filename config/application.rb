790d3d70331dc8d0903be7502f949b71a3df3e30
kazuuu <marcellokazuo@gmail.com>
2013-10-10 22:26:29 -0300

    Nao sei direito

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'net/http'
+
+require 'net/http' # Adicionei Para o Recaptcha 2013.10.09
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
