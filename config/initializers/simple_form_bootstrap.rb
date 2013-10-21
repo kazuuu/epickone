0926a7a0f92f5011642a3a877eb29ece043d1231
kazuuu <marcellokazuo@gmail.com>
2013-10-21 14:10:15 -0200

    Simple_Form: Bootstrap 3

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,15 +1,13 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
-      ba.use :input
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
-    end
-  end
+  config.wrappers :bootstrap, :tag => 'div', :class => 'row form-group', :error_class => 'has-error' do |b|
+     b.use :html5
+     b.use :placeholder
+     b.use :label
+     b.use :input, :wrap_with => { :size => "10", :tag => 'div', :class => 'controls col-xs-8 col-sm-8 col-md-8 col-lg-8' }
+     b.use :error, :wrap_with => { :tag => 'span', :class => 'help-block' }
+     b.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+   end
 
   config.wrappers :prepend, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
     b.use :html5
@@ -41,5 +39,10 @@ SimpleForm.setup do |config|
   # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
+  config.form_class = "form-horizontal"
+  config.label_class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 control-label"
+  #config.input_class = "form-control" #not yet supported
   config.default_wrapper = :bootstrap
+  config.default_input_size = 30
+
 end
